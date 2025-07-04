6d12d4b5885f0ab93a86a9e51d680e965ecdfc2b
DangQuangTien404 <dangquangtien16102002@gmail.com>
2025-07-04 19:36:40 +0700

    Add account editing page

---
+++ DangQuangTien_RazorPages/Pages/Account/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using ServiceLayer.Interfaces;
+
+namespace DangQuangTien_RazorPages.Pages.Account
+{
+    public class EditModel : PageModel
+    {
+        private readonly IAccountService _svc;
+        public EditModel(IAccountService svc) => _svc = svc;
+
+        [BindProperty]
+        public SystemAccount Account { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync(short id)
+        {
+            var role = HttpContext.Session.GetInt32("AccountRole");
+            if (role != 0)
+                return RedirectToPage("/Account/AccessDenied");
+
+            var acc = await _svc.GetByIdAsync(id);
+            if (acc == null)
+                return RedirectToPage("Index");
+
+            Account = acc;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var role = HttpContext.Session.GetInt32("AccountRole");
+            if (role != 0)
+                return RedirectToPage("/Account/AccessDenied");
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            await _svc.UpdateAsync(Account);
+            return RedirectToPage("Index");
+        }
+    }
+}
